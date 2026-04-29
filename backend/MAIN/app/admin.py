c40a62ff948057bbcc00495e6646a03df5924c09
gkdhanushgowda <gkdhanush98@gmail.com>
2026-04-29 16:29:31 +0530

    Added project collaboration models and dashboard updates

--- backend/MAIN/app/admin.py
+++ backend/MAIN/app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import AppUser
+from .models import AppUser, Project
 
 
 # Register your models here.
 
 admin.site.register(AppUser)
+admin.site.register(Project)
