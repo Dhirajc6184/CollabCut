e77bcf01efccbe6e6d98bbcf1c25916bbf38bef4
gkdhanushgowda <gkdhanush98@gmail.com>
2026-04-29 19:04:42 +0530

    Merged latest main updates

--- backend/MAIN/app/admin.py
+++ backend/MAIN/app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import AppUser, Project
+from .models import AppUser, Project, ProjectInvitation
 
 admin.site.register(AppUser)
 admin.site.register(Project)
-
+admin.site.register(ProjectInvitation)
